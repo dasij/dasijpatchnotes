f0702416bbd96be9a1baf8eda8a73d4e30ad9995
dasij <ahalanbw@hotmail.com>
2024-05-05 01:01:43 -0300

    push it

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/dasijpatchnotes/'
-},
-{
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/HOTS-dasij/'
-    : '/'
-})
+  publicPath: process.env.NODE_ENV === 'production' ? '/dasijpatchnotes/' : '/',
+});
+
