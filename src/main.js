7e653a01e268e7f8ed1df9d2abf4eeff0d4822af
dasij <ahalanbw@hotmail.com>
2024-05-05 01:13:27 -0300

    Updates

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import MainPage from './components/MainPage.vue'
-import router from './router.js'
-import './index.css'
-
-const app = createApp(MainPage)
-app.use(router)
-app.mount('#app')
