037051b4f768927df820805d3e72cacb54f5bca9
dasij <ahalanbw@hotmail.com>
2024-05-18 19:49:22 -0300

    trocando o caminho publico para o vercel

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/dasijpatchnotes/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
 });
-
